4cd0c7fe58fd9e27293bdfef941ccea731ef5569
rafdhiyaulhaq <m.rafidhiyaulhaq@gmail.com>
2024-11-30 08:23:11 +0700

    Update router and package.json for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import ModuleDetail from './pages/ModuleDetail';
 import LessonDetail from './pages/LessonDetail';
